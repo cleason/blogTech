3168b56db922d3789ce83b68b2e49e6bdf76a8d0
cleason <cocuche53@gmail.com>
2023-03-22 20:47:16 +0100

    Collectstatic

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+def index(request, *args, **kwargs):
+    posts = BlogPost.objects.all()
+    return render(request, 'index.html', {'posts': posts})
 
 def detail(request):
-    return render(request, 'detail.html')
+    posts = BlogPost.objects.all()
+    return render(request, 'detail.html', {'posts': posts})
