1dbd8d4d96690dccb448328f1e99848671b81115
cleason <cocuche53@gmail.com>
2023-03-22 17:31:12 +0100

    Second commit

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+admin.site.site_header = 'Administration du site'
+admin.site.site_title = 'CLEASON DASHBOARD'
+# admin.site.site_url = 'ahjv-togo'
+
+@admin.register(BlogPost)
+class AdminBlogPost(admin.ModelAdmin):
+    list_display = ('author', 'title', 'created_on', 'published', 'last_updated')
+    search_fields = ('title', 'content')
+    list_filter = ('author', 'title')
--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255, unique=True, verbose_name="Titre")
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    created_on = models.DateField(blank=True)
+    published = models.BooleanField(default=False, verbose_name="Publié")
+    content = models.TextField(blank=True, verbose_name="Contenu")
+    
+    def __str__(self):
+        return self.title
+
+
+    # On rajoute une classe Meta pour préciser notre modèle
+    class Meta:
+        ordering = ['-created_on']
+        verbose_name = "Article"
+
