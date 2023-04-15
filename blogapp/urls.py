5b68390abce2439c049598af7e248ad39a4f310b
cleason <cocuche53@gmail.com>
2023-04-16 01:39:43 +0200

    Mise de à jour du templates

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import BlogPostDetailView
 
 app_name = "blogapp"
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('detail/', views.detail, name="detail"),
+    path('detail/<slug:slug>/', BlogPostDetailView.as_view(), name='detail'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+from django.views.generic.detail import DetailView
 from django.shortcuts import render
 from .models import BlogPost
 
@@ -7,6 +9,11 @@ def index(request):
     posts = BlogPost.objects.all()
     return render(request, 'index.html', {'posts': posts})
 
-def detail(request):
-    posts = BlogPost.objects.all()
-    return render(request, 'detail.html', {'posts': posts})
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #context['now'] = timezone.now()
+        return context
+
+
